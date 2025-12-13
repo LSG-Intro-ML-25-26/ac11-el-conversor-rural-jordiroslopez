7f7bccb9b6fa5b37151d2165d4da3bbe2b280006
Jordi Ros López <jordi.ros@gracia.lasalle.cat>
2025-12-13 01:37:57 +0100

    feat: sistema de generacion de arboles aleatorios

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 @namespace
 class SpriteKind:
-    Interactive = SpriteKind.create()
+    Machine = SpriteKind.create()
+    Tree = SpriteKind.create()
 
 def on_down_pressed():
     animation.run_image_animation(nena,
@@ -38,15 +39,63 @@ def on_up_pressed():
         False)
 controller.up.on_event(ControllerButtonEvent.PRESSED, on_up_pressed)
 
+def generar_arbol():
+    global arboles_actuales
+    x = randint(3, 8) * 30
+    y = randint(3, 8) * 30
+    if arboles_actuales < MAX_ARBOLES:
+        sprites.create(img("""
+                ................................
+                ................................
+                ................................
+                .............ffffff.............
+                ..........fff666666fff..........
+                ........ff666888888666ff........
+                ......ff666688666688666fff......
+                .....ff6666886666668866668f.....
+                ....fff688688886688886886fff....
+                ...f8fff8666688888866778fff8f...
+                ..f8888f6666666666666777f8888f..
+                .f8888ff6666666666666677ff6688f.
+                .f8866fff66886666668866fff6668f.
+                f888668ff88888666688888ff877688f
+                f886666688888888888888888677788f
+                f886666666666688886666666667788f
+                f886666666666666666666666666688f
+                .ff6666666666666666666666666688f
+                .fff668866666666666666668866fff.
+                ..ff88886666666886666666888fff..
+                ...ff8888666668888666668888ff...
+                .....fff8866688888866688fff.....
+                ........fffffeeeeeeffffff.......
+                ......fffeeebbbbbbbbeeefff......
+                .....ffefeebbbbbbbbbbeefeff.....
+                ....fefeffebbbbbbbbbbeffefef....
+                ....fffffbeebbbeeeebeebfffff....
+                .......febbeebeebbeeebbef.......
+                ......fbeebeeeebbbbeebeebf......
+                ......ffffffffcbeeefffffff......
+                ...............cebf.............
+                ................ff..............
+                """),
+            SpriteKind.Tree).set_position(x, y)
+        arboles_actuales += 1
+arboles_actuales = 0
 nena: Sprite = None
+MAX_ARBOLES = 10
 tiles.set_current_tilemap(tilemap("""
     nivel1
     """))
 sprites.create(assets.image("""
     miImagen
-    """), SpriteKind.enemy).set_position(80, 41)
+    """), SpriteKind.Machine).set_position(56, 40)
 nena = sprites.create(assets.image("""
     nena-front
     """), SpriteKind.player)
+nena.set_position(56, 65)
 scene.camera_follow_sprite(nena)
 controller.move_sprite(nena)
+
+def on_on_update():
+    generar_arbol()
+game.on_update(on_on_update)
--- main.ts
+++ main.ts
@@ -1,42 +1,87 @@
 namespace SpriteKind {
     export const Machine = SpriteKind.create()
+    export const Tree = SpriteKind.create()
 }
-controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
-    animation.runImageAnimation(
-    nena,
-    assets.animation`nena-animation-down`,
-    500,
-    false
-    )
+
+controller.down.onEvent(ControllerButtonEvent.Pressed, function on_down_pressed() {
+    animation.runImageAnimation(nena, assets.animation`
+            nena-animation-down
+            `, 500, false)
 })
-controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
-    animation.runImageAnimation(
-    nena,
-    assets.animation`nena-animation-right`,
-    500,
-    false
-    )
+controller.right.onEvent(ControllerButtonEvent.Pressed, function on_right_pressed() {
+    animation.runImageAnimation(nena, assets.animation`
+            nena-animation-right
+            `, 500, false)
 })
-controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
-    animation.runImageAnimation(
-    nena,
-    assets.animation`nena-animation-left`,
-    500,
-    false
-    )
+controller.left.onEvent(ControllerButtonEvent.Pressed, function on_left_pressed() {
+    animation.runImageAnimation(nena, assets.animation`
+            nena-animation-left
+            `, 500, false)
 })
-controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
-    animation.runImageAnimation(
-    nena,
-    assets.animation`nena-animation-up`,
-    500,
-    false
-    )
+controller.up.onEvent(ControllerButtonEvent.Pressed, function on_up_pressed() {
+    animation.runImageAnimation(nena, assets.animation`
+            nena-animation-up
+            `, 500, false)
 })
-let nena: Sprite = null
-tiles.setCurrentTilemap(tilemap`nivel1`)
-sprites.create(assets.image`miImagen`, SpriteKind.Machine).setPosition(56, 40)
-nena = sprites.create(assets.image`nena-front`, SpriteKind.Player)
+function generar_arbol() {
+    
+    let x = randint(3, 8) * 30
+    let y = randint(3, 8) * 30
+    if (arboles_actuales < MAX_ARBOLES) {
+        sprites.create(img`
+                ................................
+                ................................
+                ................................
+                .............ffffff.............
+                ..........fff666666fff..........
+                ........ff666888888666ff........
+                ......ff666688666688666fff......
+                .....ff6666886666668866668f.....
+                ....fff688688886688886886fff....
+                ...f8fff8666688888866778fff8f...
+                ..f8888f6666666666666777f8888f..
+                .f8888ff6666666666666677ff6688f.
+                .f8866fff66886666668866fff6668f.
+                f888668ff88888666688888ff877688f
+                f886666688888888888888888677788f
+                f886666666666688886666666667788f
+                f886666666666666666666666666688f
+                .ff6666666666666666666666666688f
+                .fff668866666666666666668866fff.
+                ..ff88886666666886666666888fff..
+                ...ff8888666668888666668888ff...
+                .....fff8866688888866688fff.....
+                ........fffffeeeeeeffffff.......
+                ......fffeeebbbbbbbbeeefff......
+                .....ffefeebbbbbbbbbbeefeff.....
+                ....fefeffebbbbbbbbbbeffefef....
+                ....fffffbeebbbeeeebeebfffff....
+                .......febbeebeebbeeebbef.......
+                ......fbeebeeeebbbbeebeebf......
+                ......ffffffffcbeeefffffff......
+                ...............cebf.............
+                ................ff..............
+                `, SpriteKind.Tree).setPosition(x, y)
+        arboles_actuales += 1
+    }
+    
+}
+
+let arboles_actuales = 0
+let nena : Sprite = null
+let MAX_ARBOLES = 10
+tiles.setCurrentTilemap(tilemap`
+    nivel1
+    `)
+sprites.create(assets.image`
+    miImagen
+    `, SpriteKind.Machine).setPosition(56, 40)
+nena = sprites.create(assets.image`
+    nena-front
+    `, SpriteKind.Player)
 nena.setPosition(56, 65)
 scene.cameraFollowSprite(nena)
 controller.moveSprite(nena)
+game.onUpdate(function on_on_update() {
+    generar_arbol()
+})
