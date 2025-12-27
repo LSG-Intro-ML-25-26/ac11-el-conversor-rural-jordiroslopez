1013a27161c1bb871d73ce8f8b1f43141a42a4b6
Jordi Ros López <jordi.ros@gracia.lasalle.cat>
2025-12-27 17:15:27 +0100

    docs: agregada documentación al código

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
-# JORDI ROS LÓPEZ
+# CREADO POR JORDI ROS LÓPEZ
 
 @namespace
+# DEFINICION DE OBJETOS
 class SpriteKind:
     Tree = SpriteKind.create()
     Button = SpriteKind.create()
     Item = SpriteKind.create()
     Menu = SpriteKind.create()
     Venta = SpriteKind.create()
+
+# FUNCION PARA CREAR EL JUGADOR AL INICIO DEL JUEGO O DESPUES DE SER DESTRUIDO
 def crearPlayer(x: number, y: number):
     global nena
     nena = sprites.create(assets.image("""
@@ -16,6 +19,7 @@ def crearPlayer(x: number, y: number):
     scene.camera_follow_sprite(nena)
     controller.move_sprite(nena)
 
+# ANIMACION PLAYER HACIA ABAJO
 def on_down_pressed():
     animation.run_image_animation(nena,
         assets.animation("""
@@ -25,6 +29,7 @@ def on_down_pressed():
         False)
 controller.down.on_event(ControllerButtonEvent.PRESSED, on_down_pressed)
 
+# ANIMACION PLAYER HACIA LA DERECHA
 def on_right_pressed():
     animation.run_image_animation(nena,
         assets.animation("""
@@ -34,6 +39,7 @@ def on_right_pressed():
         False)
 controller.right.on_event(ControllerButtonEvent.PRESSED, on_right_pressed)
 
+# ANIMACION PLAYER HACIA LA IZQUIERDA
 def on_left_pressed():
     animation.run_image_animation(nena,
         assets.animation("""
@@ -43,8 +49,10 @@ def on_left_pressed():
         False)
 controller.left.on_event(ControllerButtonEvent.PRESSED, on_left_pressed)
 
+# AL PUSLAR EL BOTON "A"
 def on_a_pressed():
     global menu_maquina_activa, menu_maquina
+    # SI PLAYER ESTA JUNTO A LA MAQUINA EXPENDEDORA SE MOSTRARA EL MENU DE ESTA
     if maquina_expendedora.overlaps_with(nena) and (not (menu_maquina_activa) and not (menu_items_activo)):
         menu_maquina_activa = True
         sprites.destroy(nena)
@@ -54,7 +62,7 @@ def on_a_pressed():
             miniMenu.create_menu_item("Huevos(12 un.) = 3 troncos"),
             miniMenu.create_menu_item("Caballo = 12 troncos"),
             miniMenu.create_menu_item("Salir"))
-        
+        # ELEGIR ITEM DE LA TIENDA
         def on_button_pressed(selection, selectedIndex):
             global n_gallina, n_patata, n_cabra, n_huevos, n_caballo, menu_maquina_activa
             menu_maquina.close()
@@ -71,17 +79,20 @@ def on_a_pressed():
             crearPlayer(59, 45)
             menu_maquina_activa = False
         menu_maquina.on_button_pressed(controller.A, on_button_pressed)
-        
 controller.A.on_event(ControllerButtonEvent.PRESSED, on_a_pressed)
 
+# SI PLAYER ESTA JUNTO A UN ARBOL Y PULSA EL BOTON "A" SE TALARA EL ARBOL, SE AGREGARA UN TRONCO A LA PUNTUACION Y SE GENERARA UN NUEVO ARBOL
 def talarArbol():
     if arbol.overlaps_with(nena):
         if controller.A.is_pressed():
-            sprites.destroy_all_sprites_of_kind(SpriteKind.Tree)
+            sprites.destroy_all_sprites_of_kind(SpriteKind.Tree, effects.disintegrate, 1000)
             sprites.destroy_all_sprites_of_kind(SpriteKind.Button)
             info.change_score_by(1)
             generar_arbol()
 
+# AL PULSAR EL BOTON DE MENU SE MOSTRAR LA LISTA DE ITEMS DE PLAYER
+# PARA EVITAR BUG LO QUE HACE ES ALMACENAR LAS COORDENADAS X, Y DE PLAYER, LO DESTRUYE Y MUESTRA
+# EL MENU Y CUANDO SALE GENERA UN NUEVO PLAYER AL QUE LE ASIGNA LAS COORDENADAS ANTERIORES.
 def on_menu_pressed():
     global menu_items_activo, player_x_actual, player_y_actual, menu_items
     if not (menu_items_activo) and not (menu_maquina_activa):
@@ -114,14 +125,13 @@ def on_menu_pressed():
         
         def on_button_pressed2(selection2, selectedIndex2):
             global menu_items_activo
-            if selectedIndex2 == 5:
-                menu_items_activo = False
-                crearPlayer(player_x_actual, player_y_actual)
-                menu_items.close()
+            menu_items_activo = False
+            crearPlayer(player_x_actual, player_y_actual)
+            menu_items.close()
         menu_items.on_button_pressed(controller.A, on_button_pressed2)
-        
 controller.menu.on_event(ControllerButtonEvent.PRESSED, on_menu_pressed)
 
+# ANIMACION PLAYER HACIA ARRIBA
 def on_up_pressed():
     animation.run_image_animation(nena,
         assets.animation("""
@@ -131,6 +141,9 @@ def on_up_pressed():
         False)
 controller.up.on_event(ControllerButtonEvent.PRESSED, on_up_pressed)
 
+# DETERMINA SI LA COMPRA SE EFECTIA O NO EN FUNCION DEL PRECIO DEL ITEM SELSCCIONADO
+# SI LA PUNTUACION (TRONCOS) ES MAYOR O IGUAL AL PRECIO INTRODUCIDO, SE AGREGARA EL ITEM A PLAYER Y SE MOSTRARA UN MENSAGE DE COMPRA REALIZADA
+# EN CASO CONTRARIO MOSTRARA UN ERROR DE SALDO INSUFICIENTE.
 def efectuarCompra(núm: number):
     if info.score() >= núm:
         info.change_score_by(núm * -1)
@@ -139,10 +152,12 @@ def efectuarCompra(núm: number):
     else:
         game.splash("No tienes", "suficientes troncos")
     return 0
+
+# GENERAR UN ARBOL EN UN PUNTO ALEATORIO Y CONTROLADO DEL MAPA
 def generar_arbol():
-    global x2, y2, arbol, arboles_actuales
-    x2 = randint(1, 7) * 30
-    y2 = randint(3, 7) * 30
+    global x, y, arbol
+    x = randint(1, 7) * 30
+    y = randint(3, 7) * 30
     arbol = sprites.create(img("""
             ................................
             ................................
@@ -178,43 +193,55 @@ def generar_arbol():
             ................ff..............
             """),
         SpriteKind.Tree)
-    arbol.set_position(x2, y2)
-    arboles_actuales += 1
+    arbol.set_position(x, y)
+
+# VARIABLES
+
+
 boton_arbol: Sprite = None
 boton_maquina: Sprite = None
-y2 = 0
-x2 = 0
-menu_items: miniMenu.MenuSprite = None
+
+# COORDENADAS
+y = 0
+x = 0
 player_y_actual = 0
 player_x_actual = 0
+
+# DECLARACION DE OBJETOS
+menu_items: miniMenu.MenuSprite = None
 arbol: Sprite = None
 menu_maquina: miniMenu.MenuSprite = None
 menu_items_activo = False
 menu_maquina_activa = False
 nena: Sprite = None
 maquina_expendedora: Sprite = None
-n_caballo = 0
-n_huevos = 0
-n_cabra = 0
-n_patata = 0
-n_gallina = 0
 tiles.set_current_tilemap(tilemap("""
     nivel1
     """))
 info.set_score(0)
-arboles_actuales = 0
+maquina_expendedora = sprites.create(assets.image("""
+    miImagen
+    """), SpriteKind.Venta)
+maquina_expendedora.set_position(59, 25)
+
+# ITEMS INVENTARIO
 n_gallina = 0
 n_patata = 0
 n_cabra = 0
 n_huevos = 0
 n_caballo = 0
-maquina_expendedora = sprites.create(assets.image("""
-    miImagen
-    """), SpriteKind.Venta)
-maquina_expendedora.set_position(59, 25)
+
+# FUNCIONES
 crearPlayer(59, 45)
 generar_arbol()
 
+# MENSAGES DE INICIO
+game.show_long_text("Tala arboles y vende los troncos por cosméticos de la máquina expendedora",
+    DialogLayout.CENTER)
+game.show_long_text("Pulsa el botón de menú para ver tu inventario",
+    DialogLayout.CENTER)
+
+# FUNCION MAIN
 def on_on_update():
     global boton_maquina, menu_maquina_activa, boton_arbol
     talarArbol()
